7fef52a9b942c2823c3a72b3f4d9ca2e22aca6d8
tangentguoMacPro16 <tangentguo@tencent.com>
2022-11-23 10:15:31 +0800

    feat: 图片

--- src/image/loadImg.ts
+++ src/image/loadImg.ts
@@ -1,6 +1,7 @@
-export const loadImage = (url: string) => new Promise((resolve, reject) => {
+export const loadImage = (url: string): Promise<HTMLImageElement> =>
+  new Promise((resolve, reject) => {
     const img = new Image();
-    img.addEventListener('load', () => resolve(img));
-    img.addEventListener('error', (err) => reject(err));
+    img.addEventListener("load", () => resolve(img));
+    img.addEventListener("error", (err) => reject(err));
     img.src = url;
-});
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,159 @@
 import { Application } from "./application";
 import { loadImage } from "./image/loadImg";
-import { createAttributeSetters, createBufferInfoFromArrays, setAttributes, createVAOAndSetAttributes, drawBufferInfo } from "./utils";
+import {
+  createAttributeSetters,
+  createBufferInfoFromArrays,
+  setAttributes,
+  createVAOAndSetAttributes,
+  drawBufferInfo,
+} from "./utils";
 
 const app = new Application({
-    canvas: document.querySelector("canvas") as HTMLCanvasElement,
-    width: 800,
-    height: 600,
-    backgroundColor: '#000',
+  canvas: document.querySelector("canvas") as HTMLCanvasElement,
+  width: 800,
+  height: 600,
+  backgroundColor: "#000",
 });
 const { gl } = app;
 
 async function renderTriangles() {
-    const program = await Application.autoCreateProgram(gl, './shader/triangle.vert', './shader/triangle.frag');
-    gl.useProgram(program);
+  const program = await Application.autoCreateProgram(
+    gl,
+    "./shader/triangle.vert",
+    "./shader/triangle.frag"
+  );
+  gl.useProgram(program);
 
-    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-    
-    const vao = gl.createVertexArray();
-    gl.bindVertexArray(vao);
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    gl.vertexAttribPointer( positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
 
-    const u_color = gl.getUniformLocation(program, "u_color");
+  const vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-    document.addEventListener('click', ()=>{
-        for (let index = 0; index < 10; index++) {
-            const positions = [ Math.random(), 0, 0, Math.random(), Math.random(), 0];
-            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  const u_color = gl.getUniformLocation(program, "u_color");
 
-            gl.uniform3f(u_color, Math.random(), Math.random(), Math.random());
-            gl.drawArrays(gl.TRIANGLES, 0, 3);
-        }
-    })
+  document.addEventListener("click", () => {
+    for (let index = 0; index < 10; index++) {
+      const positions = [Math.random(), 0, 0, Math.random(), Math.random(), 0];
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(positions),
+        gl.STATIC_DRAW
+      );
 
+      gl.uniform3f(u_color, Math.random(), Math.random(), Math.random());
+      gl.drawArrays(gl.TRIANGLES, 0, 3);
+    }
+  });
 }
 
 // renderTriangles();
 
 async function renderImage() {
-    const img = await loadImage('/assets/img/apple.png');
-    const program = await Application.autoCreateProgram(gl, './shader/image.vert', './shader/image.frag');
-    gl.useProgram(program);
+  const image = await loadImage("/assets/img/apple.png");
+  const program = await Application.autoCreateProgram(
+    gl,
+    "./shader/image.vert",
+    "./shader/image.frag"
+  );
+  gl.useProgram(program);
 
-    const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const texCoordAttributeLocation = gl.getAttribLocation(program, "a_texCoord");
 
-    const texCoordAttributeLocation = gl.getAttribLocation(program, "a_texCoord");
+  // lookup uniforms
+  const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+  const imageLocation = gl.getUniformLocation(program, "u_image");
+
+  // Create a vertex array object (attribute state)
+  const vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+
+  // Create a buffer and put a single pixel space rectangle in
+  // it (2 triangles)
+  const positionBuffer = gl.createBuffer();
+
+  // Turn on the attribute
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+  // provide texture coordinates for the rectangle.
+  const texCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+    ]),
+    gl.STATIC_DRAW
+  );
+
+  // Turn on the attribute
+  gl.enableVertexAttribArray(texCoordAttributeLocation);
+  gl.vertexAttribPointer(texCoordAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+  // Create a texture.
+  const texture = gl.createTexture();
+
+  // make unit 0 the active texture uint
+  // (ie, the unit all other texture commands will affect
+  gl.activeTexture(gl.TEXTURE0 + 0);
+
+  // Bind it to texture unit 0' 2D bind point
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the parameters so we don't need mips and so we're not filtering
+  // and we don't repeat
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  // Upload the image into the texture.
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+  app.clear();
+
+  // Tell it to use our program (pair of shaders)
+  gl.useProgram(program);
+
+  // Bind the attribute/buffer set we want.
+  gl.bindVertexArray(vao);
+
+  // Pass in the canvas resolution so we can convert from
+  // pixels to clipspace in the shader
+  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+  // Tell the shader to get the texture from texture unit 0
+  gl.uniform1i(imageLocation, 0);
+
+  // Bind the position buffer so gl.bufferData that will be called
+  // in setRectangle puts data in the position buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Set a rectangle the same size as the image.
+  setRectangle(gl, 0, 0, image.width, image.height);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 
 }
 
 renderImage();
+
+function setRectangle(gl, x, y, width, height) {
+  const x1 = x;
+  const x2 = x + width;
+  const y1 = y;
+  const y2 = y + height;
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
+    gl.STATIC_DRAW
+  );
+}
